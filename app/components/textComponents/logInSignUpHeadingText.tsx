dae35a634e55016f57919c231ab70c6c343c1ca7
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-15 10:54:42 +0600

    feat: make a component of login and sign up page heading text

--- app/components/textComponents/logInSignUpHeadingText.tsx
+++ app/components/textComponents/logInSignUpHeadingText.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function logInSignUpHeadingText() {
-  return <div>logInSignUpHeadingText</div>;
-}
+type LogInSignUpHeadingTextProps = {
+  text: string;
+};
 
-export default logInSignUpHeadingText;
+export default function LogInSignUpHeadingText({
+  text,
+}: LogInSignUpHeadingTextProps) {
+  return (
+    <h1 className="font-poppins font-bold text-[38.39px] leading-[57.58px] text-headingTextColorPrimary">
+      {text}
+    </h1>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import LogInSignUpHeadingText from "./components/textComponents/logInSignUpHeadingText";
+
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
       <p>Welcome to the Synergy Classroom</p>
+      <LogInSignUpHeadingText text="Login" />
+      <LogInSignUpHeadingText text="Sign Up" />
     </>
   );
 }
