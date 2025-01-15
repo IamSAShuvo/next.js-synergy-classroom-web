455c941900719251e23b86be9920c34f621af29a
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-15 17:46:36 +0600

    refactor: make normal function to arrow function

--- app/components/textComponents/AuthDescriptionText.tsx
+++ app/components/textComponents/AuthDescriptionText.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-export default function AuthDescriptionText({
+const AuthDescriptionText = ({
   text,
   children,
 }: {
   text: string;
   children?: React.ReactNode;
-}) {
+}) => {
   return (
     <h1 className="font-poppins font-normal text-[14.39px] leading-[21.59px] text-descriptionColor">
       {text} {children}
     </h1>
   );
-}
+};
+
+export default AuthDescriptionText;
--- app/components/textComponents/AuthHeadingText.tsx
+++ app/components/textComponents/AuthHeadingText.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function AuthHeadingText({ text }: { text: string }) {
+const AuthHeadingText = ({ text }: { text: string }) => {
   return (
     <h1 className="font-poppins font-bold text-[38.39px] leading-[57.58px] text-headingColor">
       {text}
     </h1>
   );
-}
+};
+
+export default AuthHeadingText;
