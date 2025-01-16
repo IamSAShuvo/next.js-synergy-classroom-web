ea9389697c07ee5e41455c27500d6bc4bb9030a5
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-16 10:39:02 +0600

    refactor: bg color remove

--- app/components/textComponents/DashboardCardTitle.tsx
+++ app/components/textComponents/DashboardCardTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DashboardCardTitle = ({ text }: { text: string }) => {
   return (
-    <h1 className="font-poppins font-medium text-2xl leading-9 text-white bg-slate-800">
+    <h1 className="font-poppins font-medium text-2xl leading-9 text-white">
       {text}
     </h1>
   );
