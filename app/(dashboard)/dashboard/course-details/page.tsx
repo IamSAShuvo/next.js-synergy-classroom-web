3061f1e0a7a681e26be9f5315d4e593d2725934d
Salman Aziz <salmanazizshuvo@gmail.com>
2025-01-28 20:00:41 +0600

    feat: add course details page with tab navigation component

---
+++ app/(dashboard)/dashboard/course-details/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicTabs from "./components/CourseTab";
+
+const page = () => {
+  return (
+    <div>
+      <BasicTabs />
+    </div>
+  );
+};
+
+export default page;
