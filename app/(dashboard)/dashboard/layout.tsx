b46d053ffed8bcb0f503fbfece010da7b10170e9
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-30 01:19:54 +0600

    refactor: restructure DashboardLayout for improved layout and add Logout button

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,33 +1,46 @@
 import SidebarMenuButton from "@/app/components/Buttons/SidebarMenuButton";
 import NavBar from "@/app/components/NavBar/NavBar";
 import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-export default function authLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <NavBar />
-      <main className="flex">
-        <aside className="border-r-2 border-gray-200 p-6 h-[83vh]">
-          <h1>Dashboard</h1>
-          <ul>
-            <li>
+    <div className="flex flex-col h-screen">
+      {/* Navbar at the top */}
+      <div className="w-full border-b border-gray-200 relative">
+        <NavBar />
+      </div>
+
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar */}
+        <aside className="w-64 bg-white border-r border-gray-200 px-4 flex flex-col justify-between h-[calc(100vh-4rem)] sticky top-[4rem]">
+          <div className="flex-1 overflow-hidden">
+            <nav className="mt-6">
               <SidebarMenuButton
                 icon={HomeIcon}
                 text="Home"
                 trailingIcon={ChevronRightIcon}
+                className="w-full bg-skyBlue hover:bg-indigo-600 text-white px-7 py-5 rounded-full font-medium text-sm leading-3 flex justify-between"
               />
-            </li>
-            <li>Profile</li>
-            <li>Settings</li>
-          </ul>
+            </nav>
+          </div>
+          <div className="pb-24">
+            <SidebarMenuButton
+              icon={LogoutIcon}
+              text="Log Out"
+              className="w-full hover:border-2 border-cosmicBlue text-cosmicBlue px-7 py-5 rounded-full font-medium text-sm leading-3 flex justify-between"
+            />
+          </div>
         </aside>
-        <div>{children}</div>
-      </main>
+
+        {/* Main Content */}
+        <div className="flex-1 bg-white overflow-auto">{children}</div>
+      </div>
     </div>
   );
 }
