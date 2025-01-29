d3129f4e7f8527bc2bbc4c2ea58418500b550099
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-30 01:20:20 +0600

    refactor: streamline NavBar structure and enhance logo size for better visibility

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -5,24 +5,16 @@ import NavBarProfile from "../UserProfile/NavBarProfile";
 const NavBar = () => {
   const role = ["Software Engineer", "Frontend Developer", "Backend Developer"];
   return (
-    <div>
-      <header className="border-b-2 border-gray-200">
-        <nav className="flex justify-between items-center p-6">
-          <Image src="/logo.svg" alt="logo" width={100} height={100} />
-          <NavBarProfile
-            roles={role}
-            avatarSrc="/my_profile.jpeg"
-            name="Salman Aziz"
-            avatarHeight={50}
-            avatarWidth={50}
-          />
-        </nav>
-      </header>
-      <main>
-        <aside></aside>
-        <section></section>
-      </main>
-    </div>
+    <nav className="flex justify-between items-center p-6">
+      <Image src="/logo.svg" alt="logo" width={180} height={100} />
+      <NavBarProfile
+        roles={role}
+        avatarSrc="/my_profile.jpeg"
+        name="Salman Aziz"
+        avatarHeight={50}
+        avatarWidth={50}
+      />
+    </nav>
   );
 };
 
