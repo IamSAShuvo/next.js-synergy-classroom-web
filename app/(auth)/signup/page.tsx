4dd77bdf7d920a3472a4439400f68ffc53fa1fcd
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-01-30 12:21:53 +0600

    refactor: extract signup form into separate SignupMain component for better organization

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,69 +1,8 @@
-import PrimaryButton from "@/app/components/Buttons/PrimaryButton";
-import RadioButtons from "@/app/components/Buttons/RadioButtons";
-import PrimaryCard from "@/app/components/CardComponent/PrimaryCard";
-import PrimaryInputField from "@/app/components/inputFields/PrimaryInputField";
-import LinkText from "@/app/components/links/LinkText";
-import DescriptionText from "@/app/components/typography/DescriptionText";
-import PrimaryHeading from "@/app/components/typography/PrimaryHeading";
+import SignupMain from "@/app/components/Auth/SignupMain";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <PrimaryCard
-        className="max-w-lg mx-auto bg-white rounded-2xl shadow-lg p-8"
-        header={
-          <>
-            <PrimaryHeading text="Sign Up" />
-            <DescriptionText
-              text="Enter your details below & free sign up"
-              color="text-secondaryColor"
-              fontSize="text-sm"
-              lineHeight="leading-6"
-            />
-          </>
-        }
-        content={
-          <>
-            <PrimaryInputField
-              label="Username"
-              placeholder="Enter Your UserName"
-              variant="outlined"
-            />
-            <PrimaryInputField
-              label="Name"
-              placeholder="Enter Your Name"
-              variant="outlined"
-            />
-            <PrimaryInputField label="Password" isPassword variant="outlined" />
-            <PrimaryInputField
-              label="Email"
-              placeholder="Please Provide Your Email"
-              variant="outlined"
-            />
-            <div>
-              <RadioButtons />
-            </div>
-          </>
-        }
-        footer={
-          <DescriptionText
-            text="Already have an account?"
-            color="text-secondaryColor"
-            fontSize="text-sm"
-            lineHeight="leading-6"
-          >
-            <LinkText url="/login" text="Login" />
-          </DescriptionText>
-        }
-      >
-        <PrimaryButton
-          text="Sign Up"
-          className="w-full hover:bg-indigo-600 bg-skyBlue px-7 py-3 rounded text-white font-medium text-base leading-6"
-        />
-      </PrimaryCard>
-    </div>
-  );
+  return <SignupMain />;
 };
 
 export default page;
