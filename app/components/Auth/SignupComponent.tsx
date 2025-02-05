ff1cc3e28fab07f8de29c9681c25d6a211230bce
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-05 18:19:00 +0600

    refactor: improve styling and layout of SignupComponent for better responsiveness

--- app/components/Auth/SignupComponent.tsx
+++ app/components/Auth/SignupComponent.tsx
@@ -84,15 +84,13 @@ const SignupComponent = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <PrimaryCard
-        className="w-[25%] mx-auto bg-white rounded-2xl shadow-lg p-8"
+        className="max-w-lg mx-auto bg-white rounded-2xl shadow-lg p-8"
         header={
           <>
             <PrimaryHeading text="Sign Up" />
             <DescriptionText
               text="Enter your details below & free sign up"
-              color="text-secondaryColor"
-              fontSize="text-sm"
-              lineHeight="leading-6"
+              className="text-secondaryColor text-sm leading-6"
             />
           </>
         }
@@ -141,9 +139,7 @@ const SignupComponent = () => {
         footer={
           <DescriptionText
             text="Already have an account?"
-            color="text-secondaryColor"
-            fontSize="text-sm"
-            lineHeight="leading-6"
+            className="text-secondaryColor text-sm leading-6"
           >
             <LinkText
               className="font-normal text-sm leading-5 text-skyBlue hover:underline "
