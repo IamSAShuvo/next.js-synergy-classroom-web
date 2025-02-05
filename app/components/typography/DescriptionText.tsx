a66bfebf89a8ce8ff2606481f5773f314a3b4181
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-05 18:19:08 +0600

    refactor: simplify DescriptionWithChildren component props and improve className handling

--- app/components/typography/DescriptionText.tsx
+++ app/components/typography/DescriptionText.tsx
@@ -1,37 +1,19 @@
 import React, { FC } from "react";
 
 interface DescriptionWithChildrenProps {
-  text: string;
-  color: string;
-  fontSize: string;
-  lineHeight?:
-    | "leading-normal"
-    | "leading-loose"
-    | "leading-tight"
-    | "leading-snug"
-    | "leading-none"
-    | "leading-extra-loose"
-    | "leading-5"
-    | "leading-6"
-    | "leading-7";
-  letterSpacing?: string;
+  text?: string;
+
   children?: React.ReactNode;
+  className?: string;
 }
 
 const DescriptionWithChildren: FC<DescriptionWithChildrenProps> = ({
   text,
-  color,
-  fontSize,
   children,
-  lineHeight,
-  letterSpacing = "tracking-normal",
+  className = "",
 }) => {
   return (
-    <p
-      className={`font-normal ${color} ${lineHeight} ${
-        letterSpacing || ""
-      } ${fontSize}`}
-    >
+    <p className={className}>
       {text} {children && <span>{children}</span>}
     </p>
   );
