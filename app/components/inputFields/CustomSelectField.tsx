fc9f16b50a7e18412d4d5565008137a05fe6c14f
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-11 10:30:42 +0600

    chore: Move Styles Outside the Component

--- app/components/inputFields/CustomSelectField.tsx
+++ app/components/inputFields/CustomSelectField.tsx
@@ -1,3 +1,77 @@
+// import { MenuItem, TextField } from "@mui/material";
+// import React, { FC } from "react";
+// import USER_ROLE_OPTIONS from "@/app/constants/professionOptions";
+
+// interface Option {
+//   value: string;
+//   label: string;
+// }
+
+// interface CustomSelectFieldProps {
+//   value: string;
+//   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//   label?: string;
+//   helperText?: string;
+//   options?: Option[];
+// }
+
+// const CustomSelectField: FC<CustomSelectFieldProps> = ({
+//   value,
+//   onChange,
+//   label = "Profession",
+//   helperText = "Please select your Role",
+//   options = USER_ROLE_OPTIONS,
+// }) => {
+//   const inputFieldStyle = {
+//     "& .MuiInputLabel-root": {
+//       fontFamily: "Poppins, sans-serif",
+//       color: "var(--primaryColor)",
+//     },
+//     "& .MuiOutlinedInput-root": {
+//       fontFamily: "Poppins, sans-serif",
+//       "& fieldset": {
+//         borderColor: "var(--primaryColor)",
+//       },
+//       "&:hover fieldset": {
+//         borderColor: "var(--primaryColor)",
+//       },
+//       "&.Mui-focused fieldset": {
+//         borderColor: "var(--primaryColor)",
+//       },
+//     },
+//     "& .MuiFormHelperText-root": {
+//       fontFamily: "Poppins, sans-serif",
+//       color: "var(--primaryColor)",
+//     },
+//   };
+
+//   const menuItemStyle = {
+//     fontFamily: "Poppins, sans-serif",
+//     color: "var(--primaryColor)",
+//   };
+
+//   return (
+//     <TextField
+//       className="w-full"
+//       sx={inputFieldStyle}
+//       id="outlined-select-currency"
+//       select
+//       label={label}
+//       value={value} // Controlled component
+//       onChange={onChange} // Handle change to update value
+//       helperText={helperText}
+//     >
+//       {options.map((option) => (
+//         <MenuItem sx={menuItemStyle} key={option.value} value={option.value}>
+//           {option.label}
+//         </MenuItem>
+//       ))}
+//     </TextField>
+//   );
+// };
+
+// export default CustomSelectField;
+
 import { MenuItem, TextField } from "@mui/material";
 import React, { FC } from "react";
 import USER_ROLE_OPTIONS from "@/app/constants/professionOptions";
@@ -15,6 +89,34 @@ interface CustomSelectFieldProps {
   options?: Option[];
 }
 
+const inputFieldStyle = {
+  "& .MuiInputLabel-root": {
+    fontFamily: "Poppins, sans-serif",
+    color: "var(--primaryColor)",
+  },
+  "& .MuiOutlinedInput-root": {
+    fontFamily: "Poppins, sans-serif",
+    "& fieldset": {
+      borderColor: "var(--primaryColor)",
+    },
+    "&:hover fieldset": {
+      borderColor: "var(--primaryColor)",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "var(--primaryColor)",
+    },
+  },
+  "& .MuiFormHelperText-root": {
+    fontFamily: "Poppins, sans-serif",
+    color: "var(--primaryColor)",
+  },
+};
+
+const menuItemStyle = {
+  fontFamily: "Poppins, sans-serif",
+  color: "var(--primaryColor)",
+};
+
 const CustomSelectField: FC<CustomSelectFieldProps> = ({
   value,
   onChange,
@@ -22,34 +124,6 @@ const CustomSelectField: FC<CustomSelectFieldProps> = ({
   helperText = "Please select your Role",
   options = USER_ROLE_OPTIONS,
 }) => {
-  const inputFieldStyle = {
-    "& .MuiInputLabel-root": {
-      fontFamily: "Poppins, sans-serif",
-      color: "var(--primaryColor)",
-    },
-    "& .MuiOutlinedInput-root": {
-      fontFamily: "Poppins, sans-serif",
-      "& fieldset": {
-        borderColor: "var(--primaryColor)",
-      },
-      "&:hover fieldset": {
-        borderColor: "var(--primaryColor)",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "var(--primaryColor)",
-      },
-    },
-    "& .MuiFormHelperText-root": {
-      fontFamily: "Poppins, sans-serif",
-      color: "var(--primaryColor)",
-    },
-  };
-
-  const menuItemStyle = {
-    fontFamily: "Poppins, sans-serif",
-    color: "var(--primaryColor)",
-  };
-
   return (
     <TextField
       className="w-full"
@@ -57,8 +131,8 @@ const CustomSelectField: FC<CustomSelectFieldProps> = ({
       id="outlined-select-currency"
       select
       label={label}
-      value={value} // Controlled component
-      onChange={onChange} // Handle change to update value
+      value={value}
+      onChange={onChange}
       helperText={helperText}
     >
       {options.map((option) => (
