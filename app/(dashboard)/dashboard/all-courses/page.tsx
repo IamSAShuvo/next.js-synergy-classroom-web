fb2962edbee50703533ac017edfc67ee7e7fc2b2
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-15 14:27:19 +0600

    chore: delete images from public

--- app/(dashboard)/dashboard/all-courses/page.tsx
+++ app/(dashboard)/dashboard/all-courses/page.tsx
@@ -1,7 +1,12 @@
+import DashboardComponent from "@/app/components/DashboardMainContent/DashboardComponent";
 import React from "react";
 
 const page = () => {
-  return <div>See all courses</div>;
+  return (
+    <div className="flex flex-col mx-7 my-6 gap-4">
+      <DashboardComponent />
+    </div>
+  );
 };
 
 export default page;
