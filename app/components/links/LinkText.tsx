fe0aa562f9ae7c42f9d1a83d30bb1a0dfa9f949c
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-17 12:36:17 +0600

    chore: explicitly passing passHref

--- app/components/links/LinkText.tsx
+++ app/components/links/LinkText.tsx
@@ -4,12 +4,17 @@ import React, { FC } from "react";
 interface LinkTextProps {
   url: string;
   text: string;
+  newTab?: boolean;
+  isExternal?: boolean;
 }
 
-const LinkText: FC<LinkTextProps> = ({ url, text }) => {
+const LinkText: FC<LinkTextProps> = ({ url, text, newTab, isExternal }) => {
   return (
     <Link
       href={url}
+      passHref
+      rel={newTab || isExternal ? "noopener noreferrer" : undefined}
+      target={newTab || isExternal ? "_blank" : undefined}
       className="font-normal text-sm leading-5 text-skyBlue hover:underline"
     >
       {text}
