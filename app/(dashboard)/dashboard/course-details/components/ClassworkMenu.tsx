a0307577f81173497fcb0ae3e7ae91ad4e6e3ef2
IamSAShuvo <salmanazizshuvo@gmail.com>
2025-02-19 09:45:13 +0600

    chore: remove comment

--- app/(dashboard)/dashboard/course-details/components/ClassworkMenu.tsx
+++ app/(dashboard)/dashboard/course-details/components/ClassworkMenu.tsx
@@ -64,29 +64,6 @@ export default function ClassworkMenu() {
       <CustomTabPanel value={value} index={0}>
         <div className="p-6 flex flex-col gap-8">
           <SectionHeader courseData={courseData} />
-          {/* <section className="flex flex-col gap-8 p-5 border-2 rounded-md border-gray-300">
-            <SecondaryProfile
-              name={courseData.teacherName}
-              avatarSrc="/profile_avatar.png"
-              avatarHeight={60}
-              avatarWidth={60}
-              shouldOpenModal={false}
-              className="flex items-center gap-3 text-xl font-normal leading-5 text-midnightBlack"
-            />
-            <div className="space-y-5">
-              <DescriptionText
-                text="Book List"
-                color="text-steelBlue"
-                fontSize="text-xl"
-                lineHeight="leading-5"
-              />
-              <ItemList
-                items={courseData.books.map((book) => book.name)}
-                className="text-secondaryColor font-normal text-base leading-5"
-                useNumber={true}
-              />
-            </div>
-          </section> */}
           <CourseDetailsSection courseData={courseData} />
         </div>
       </CustomTabPanel>
